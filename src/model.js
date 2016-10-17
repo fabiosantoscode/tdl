7dc74f7f0b36b9603d1f62aa819bbd1730ae31df
Fábio Santos <fabiosantosart@gmail.com>
2016-10-18 00:14:25 +0100

    Prevent Object.assign from mutating things

--- src/model.js
+++ src/model.js
@@ -9,36 +9,36 @@ module.exports = {
   },
   reducers: {
     updateTitle(title, state) {
-      return Object.assign(state, { title });
+      return Object.assign({}, state, { title });
     },
     updateClock(clock, state) {
-      return Object.assign(state, { clock });
+      return Object.assign({}, state, { clock });
     },
     addItem(itemDesc, state) {
       const items = state.items.concat([{desc:itemDesc, done:false}]);
-      return Object.assign(state, { items });
+      return Object.assign({}, state, { items });
     },
     deleteItem(itemIndex, state) {
       const items = state.items.filter((item, idx) => {
         return itemIndex !== idx;
       });
-      return Object.assign(state, { items });
+      return Object.assign({}, state, { items });
     },
     deleteDoneItems(_, state) {
       const items = state.items.filter(item => !item.done);
-      return Object.assign(state, { items });
+      return Object.assign({}, state, { items });
     },
     toggleItemDone(itemIndex, state) {
       const items = state.items.map((item, idx) => {
-        return (idx === itemIndex) ? Object.assign(item, { done:!item.done }) : item;
+        return (idx === itemIndex) ? Object.assign({}, item, { done:!item.done }) : item;
       });
-      return Object.assign(state, { items });
+      return Object.assign({}, state, { items });
     },
     updateItemDesc(o, state) {
       const items = state.items.map((item, idx) => {
-        return (idx === o.itemIndex) ? Object.assign(item, { desc:o.desc }) : item;
+        return (idx === o.itemIndex) ? Object.assign({}, item, { desc:o.desc }) : item;
       });
-      return Object.assign(state, { items });
+      return Object.assign({}, state, { items });
     },
     effects: {
       fetch: (data, state, send, done) => {
